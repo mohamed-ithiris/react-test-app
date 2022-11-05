29d0cb89f2e7190f8ff17d35e6ffe341adc6664e
Mohamed Ithiris <ithris17@gmail.com>
2022-11-05 23:50:57 +0530

    blueprintjs and uuidv4 installed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { TodoBox } from './components/TodoBox';
 
 function App() {
+  const [todos, setTodos] = useState(["complete project", "do work"]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoBox todoValue={todos} />
     </div>
   );
 }
